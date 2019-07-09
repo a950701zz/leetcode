25dd8f08bac705edcb22eb22fbe3cdacf7266106
yangzhirui <568938548@qq.com>
2019-07-09 21:24:36 +0800

    前K 个高频元素

--- Leetcode/源.cpp
+++ Leetcode/源.cpp
@@ -2119,42 +2119,81 @@ struct TreeNode {
 //	}
 //};
 
-struct TreeNode {
-	     int val;
-	     TreeNode *left;
-	     TreeNode *right;
-	     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-	 };
-
-//二叉搜索树的中序遍历
+//struct TreeNode {
+//	     int val;
+//	     TreeNode *left;
+//	     TreeNode *right;
+//	     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//	 };
+//
+////二叉搜索树的中序遍历
+//class Solution {
+//public:
+//	int current; int previous; int rst = true;
+//	bool isValidBST(TreeNode* root) {
+//		current = root->val;
+//		middleBFS(root);
+//		return rst;
+//	}
+//
+//	bool middleBFS(TreeNode* root)
+//	{
+//		if (rst == false) return false;
+//		if (root == NULL) return;
+//		if (root->left != NULL) middleBFS(root->left);
+//		previous = current;
+//		current = root->val;
+//		if (current < previous) rst = false;
+//		if (root->right != NULL) middleBFS(root->right);
+//		return true;
+//	}
+//};
+//
 class Solution {
 public:
-	int current; int previous; int rst = true;
-	bool isValidBST(TreeNode* root) {
-		current = root->val;
-		middleBFS(root);
-		return rst;
-	}
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		int n = nums1.size();
+		int m = nums2.size();
 
-	bool middleBFS(TreeNode* root)
-	{
-		if (rst == false) return false;
-		if (root == NULL) return;
-		if (root->left != NULL) middleBFS(root->left);
-		previous = current;
-		current = root->val;
-		if (current < previous) rst = false;
-		if (root->right != NULL) middleBFS(root->right);
-		return true;
+		if (n > m)  //保证数组1一定最短
+		{
+			return findMedianSortedArrays(nums2, nums1);
+		}
+
+		// Ci 为第i个数组的割,比如C1为2时表示第1个数组只有2个元素。LMaxi为第i个数组割后的左元素。RMini为第i个数组割后的右元素。
+		int LMax1, LMax2, RMin1, RMin2, c1, c2, lo = 0, hi = 2 * n;  //我们目前是虚拟加了'#'所以数组1是2*n长度
+
+		while (lo <= hi)   //二分
+		{
+			c1 = (lo + hi) / 2;  //c1是二分的结果
+			c2 = m + n - c1;
+
+			LMax1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
+			RMin1 = (c1 == 2 * n) ? INT_MAX : nums1[c1 / 2];
+			LMax2 = (c2 == 0) ? INT_MIN : nums2[(c2 - 1) / 2];
+			RMin2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
+
+			if (LMax1 > RMin2)
+				hi = c1 - 1;
+			else if (LMax2 > RMin1)
+				lo = c1 + 1;
+			else
+				break;
+		}
+		return (max(LMax1, LMax2) + min(RMin1, RMin2)) / 2.0;
 	}
 };
 
 
-int main() {
-	vector<vector<int>> or = { { 0, 2 } };
-	Solution t;
-}
+int main(int argc, char *argv[])
+{
+	vector<int> nums1 = { 2, 3, 5 };
+	vector<int> nums2 = { 1, 4, 7, 9 };
 
+	Solution solution;
+	double ret = solution.findMedianSortedArrays(nums1, nums2);
+	return 0;
+}
 
 
 
